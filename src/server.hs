9680c2c8567d47a96bf1ef47c35871ba0dc261e0
Will Sewell <me@willsewell.com>
2015-05-03 18:53:27 +0100

    Now respond with a random user.

--- src/server.hs
+++ src/server.hs
@@ -7,17 +7,17 @@ import Control.Applicative ((<$>), (<*>))
 import Data.Aeson ((.=))
 import Data.ConfigFile (OptionSpec, emptyCP, items, readfile)
 import Data.Either.Utils (forceEither)
+import Data.Monoid ((<>))
 import Database.PostgreSQL.Simple
-  ( Only(Only)
-  , connect
+  ( connect
   , connectDatabase
   , connectPassword
   , connectUser
   , defaultConnectInfo
-  , query
+  , query_
   )
 import Database.PostgreSQL.Simple.FromRow (FromRow(fromRow), field)
-import Snap.Core (Snap, getParam, ifTop,writeBS)
+import Snap.Core (Snap, getParam, ifTop, writeBS)
 import Snap.Http.Server (quickHttpServe)
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as BL
@@ -43,19 +43,21 @@ instance A.ToJSON User where
 
 main :: IO ()
 main = do
-  xs <- getUsers
-  quickHttpServe $ site xs
+  u <- getRandomUser
+  quickHttpServe $ site u
 
-getUsers :: IO [User]
-getUsers = do
+getRandomUser :: IO User
+getRandomUser = do
   dbCfg <- readDbCfg
   conn <- connect defaultConnectInfo
     { connectDatabase = getCfgItem "database" dbCfg
     , connectUser = getCfgItem "user" dbCfg
     , connectPassword = getCfgItem "password" dbCfg
     }
-  query conn "SELECT username, cur_profile_pic FROM users WHERE username=?" $
-    Only ("fractalsea" :: String)
+  res <- query_ conn $
+    "SELECT username, cur_profile_pic FROM users "
+    <> "OFFSET RANDOM() * (SELECT COUNT(*) FROM users) LIMIT 1"
+  return $ head res
 
 readDbCfg :: IO [(OptionSpec, String)]
 readDbCfg = do
@@ -66,10 +68,10 @@ readDbCfg = do
 getCfgItem :: OptionSpec -> [(OptionSpec, String)] -> String
 getCfgItem k xs = snd $ head $ filter (\(x, _) -> x == k) xs
 
-site :: [User] -> Snap ()
-site users = do
+site :: User -> Snap ()
+site user = do
   Just param <- getParam "callback"
   writeBS param
   writeBS "("
-  ifTop $ writeBS $ BL.toStrict $ A.encode $ head users
+  ifTop $ writeBS $ BL.toStrict $ A.encode user
   writeBS ")"
